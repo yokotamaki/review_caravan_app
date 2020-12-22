f05cde6d9a74f14bb2a331bc4542e991df82d78f
yokotamaki <ec2-user@ip-172-31-36-175.ec2.internal>
2020-12-22 07:31:31 +0000

    一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'blogs/index'
+  # get 'blogs/show'
+  # get 'blogs/new'
+  # get 'blogs/edit'
+  resources :blogs
 end
