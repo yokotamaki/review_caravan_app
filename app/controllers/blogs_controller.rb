c9ece8ae1afa6e13dfd2a3893da51c68e5f832c6
yokotamaki <ec2-user@ip-172-31-36-175.ec2.internal>
2020-12-22 08:24:14 +0000

    Rubocop

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,4 @@
 class BlogsController < ApplicationController
-  
   def index
     @blogs = Blog.all
   end
@@ -11,7 +10,7 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -21,20 +20,21 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
-    redirect_to blogs_path  
+    redirect_to blogs_path
   end
-  
+
   private
+
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
